f392a81bca5c5aee331d56a81a9594f5fc8d9dcc
paperdeer <tjrgus0713@gmail.com>
2023-11-27 23:30:01 +0900

    feat: 랜딩페이지 첫번째 섹션 추가

---
+++ src/components/Landing/FirstSection.tsx
@@ -0,0 +1,12 @@
+import LandingLayout from './Layout';
+
+// TODO: 애니메이션 추가
+const FirstSection = () => {
+  return (
+    <LandingLayout backgroundColor="skyblue">
+      <image />
+    </LandingLayout>
+  );
+};
+
+export default FirstSection;
