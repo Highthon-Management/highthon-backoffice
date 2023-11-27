90dd4b6bd99d399fbfa6abd6bac92cd0f29fa82a
paperdeer <tjrgus0713@gmail.com>
2023-11-27 23:33:01 +0900

    feat: 랜딩페이지 각 섹션 합치기 및 scrollSnap 자동 적용되도록 처리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
-const LandingPage = () => <div>landingPage</div>;
+import styled from 'styled-components';
+import FirstSection from '../components/Landing/FirstSection';
+import SecondSection from '../components/Landing/SecondSection';
+import ThirdSection from '../components/Landing/ThirdSection';
+import FourthSection from '../components/Landing/FourthSection';
+import FifthSection from '../components/Landing/FifthSection';
+
+const LandingPage = () => (
+  <Wrapper>
+    <FirstSection />
+    <SecondSection />
+    <ThirdSection />
+    <FourthSection />
+    <FifthSection />
+  </Wrapper>
+);
 
 export default LandingPage;
+
+const Wrapper = styled.section`
+  overflow: scroll;
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  > * {
+    scroll-snap-align: start;
+    flex: none;
+  }
+`;
