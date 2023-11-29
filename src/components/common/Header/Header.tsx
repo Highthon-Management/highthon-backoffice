3804b3330b7a6cc68edbb33eb0dc31b989cf243c
je355 <chaeeehui@gmail.com>
2023-11-29 21:54:10 +0900

    📑 ::	헤더 추가

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import hightonLogo from '../image/hightonLogo.svg';
+import { StyledHeader, StyledHeaderImg, StyledBookBox, StyledBookText } from '../../../styles/Header';
+
+const Header: React.FC = () => {
+  return (
+    <StyledHeader>
+      <div className="allHeader">
+        <StyledHeaderImg src={hightonLogo} className="headerImg" alt="Header Logo" />
+        <StyledBookBox>
+          <StyledBookText>신청 바로하기</StyledBookText>
+        </StyledBookBox>
+      </div>
+      <link href="https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/static/woff2/SUIT.css" rel="stylesheet" />
+    </StyledHeader>
+  );
+};
+
+export default Header;
